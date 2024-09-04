fdee9d7ae1f544f327f9b0903843ff0201537200
DamianSubzda <DamianSubzda@wp.pl>
2024-09-04 15:47:50 +0200

    Added full screen photo display

--- wedding-photo-client/src/components/gallery/Photo.js
+++ wedding-photo-client/src/components/gallery/Photo.js
@@ -1,26 +1,58 @@
-import './Photo.css';
+import { useState } from "react";
+import "./Photo.css";
+import { BsFillXCircleFill } from "react-icons/bs";
 
 function Photo({ photo }) {
-    
-    const formattedDate = new Date(photo.createdAt).toLocaleDateString('en-GB', {
-        day: '2-digit',
-        month: '2-digit'
-    });
+  const [isFullscreen, setIsFullscreen] = useState(false);
 
-    const formattedTime = new Date(photo.createdAt).toLocaleTimeString('en-GB', {
-        hour: '2-digit',
-        minute: '2-digit'
-    });
+  const formattedDate = new Date(photo.createdAt).toLocaleDateString("en-GB", {
+    day: "2-digit",
+    month: "2-digit",
+  });
 
-    return (
-        <div className="photo-container">
-            <img className='photo' src={photo.fullFilePath} alt={`${photo.fileName}`} />
-            <div className="photo-caption">
-                <p className="photo-time">{formattedDate} {formattedTime}</p>
-                <p className="photo-description">{photo.description}</p>
-            </div>
+  const formattedTime = new Date(photo.createdAt).toLocaleTimeString("en-GB", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  const hidePhoto = () => {
+    setIsFullscreen(false);
+  };
+
+  const showPhoto = () => {
+    setIsFullscreen(true);
+  };
+
+  return (
+    <>
+      <div className="photo-container">
+        <img
+          className="photo"
+          src={photo.fullFilePath}
+          alt={`${photo.fileName}`}
+          onClick={showPhoto}
+        />
+        <div className="photo-caption">
+          <p className="photo-time">
+            {formattedDate} {formattedTime}
+          </p>
+          <p className="photo-description">{photo.description}</p>
         </div>
-    );
+      </div>
+      {isFullscreen &&
+      (
+        <div className="full-screen" onClick={hidePhoto}>
+          <BsFillXCircleFill className="hide-button" onClick={hidePhoto} />
+          <img
+            className="photo"
+            src={photo.fullFilePath}
+            alt={`${photo.fileName}`}
+            onClick={(e) => e.stopPropagation()}
+          />
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Photo;
